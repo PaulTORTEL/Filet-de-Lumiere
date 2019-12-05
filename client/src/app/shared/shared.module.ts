be7021774d404743dea803d49a6f8e6d83d9f6d9
PaulTORTEL <ptortel@free.fr>
2019-12-05 23:11:18 +0100

    [Front] Mobile version for the navbar

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -5,9 +5,10 @@ import { NgZorroAntdModule } from "ng-zorro-antd";
 import { LoginComponent } from "./components/login/login.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { UserNavCardComponent } from './components/navbar/user-nav-card/user-nav-card.component';
+import { NotificationComponent } from './components/navbar/notification/notification.component';
 
 @NgModule({
-  declarations: [NavbarComponent, LoginComponent, UserNavCardComponent],
+  declarations: [NavbarComponent, LoginComponent, UserNavCardComponent, NotificationComponent],
   imports: [CommonModule, NgZorroAntdModule, ReactiveFormsModule],
   exports: [NavbarComponent, NgZorroAntdModule],
   entryComponents: [LoginComponent]
